87ea42a32d600bd972c0fa0d07670a849d8fcbaa
zpye <986790855@qq.com>
2019-12-08 23:48:37 +0800

    update cmake

--- main.cpp
+++ main.cpp
@@ -1,105 +1,101 @@
-#include <CL/opencl.h>
 #include <CL/cl.hpp>
 
 #include <iostream>
 #include <vector>
 
 #include "ClUtils.h"
-#include "PI.hpp"
+// #include "PI.hpp"
 
 using namespace std;
 using namespace cl;
 
-int main()
-{
-    vector<Platform> vp;
-    vector<Device> vd;
+int main() {
+  vector<Platform> vp;
+  vector<Device> vd;
 
-    int err = CL_SUCCESS;
+  int err = CL_SUCCESS;
 
-    // get platforms
-    checkCLError(Platform::get(&vp));
-    printf("platform size = %lld\n", vp.size());
-    for(int i = 0; i < vp.size(); ++i)
-    {
-        string info;
-        vp[i].getInfo(CL_PLATFORM_NAME, &info);
-        printf("%s\n", info.c_str());
-    }
+  // get platforms
+  checkCLError(Platform::get(&vp));
+  printf("platform size = %lld\n", vp.size());
+  for (int i = 0; i < vp.size(); ++i) {
+    string info;
+    vp[i].getInfo(CL_PLATFORM_NAME, &info);
+    printf("%s\n", info.c_str());
+  }
 
-    // get devices
-    vp[1].getDevices(CL_DEVICE_TYPE_ALL, &vd);
-    printf("device number = %lld\n", vd.size());
-    for(int i = 0; i < vd.size(); ++i)
-    {
-        string info;
-        vd[i].getInfo(CL_DEVICE_NAME, &info);
-        printf("%s\n", info.c_str());
-    }
-    
-    // get context
-    cl_context_properties properties[] = {CL_CONTEXT_PLATFORM, 
-        (cl_context_properties)(vp[1])(), 0};
-    Context context(vd, properties, NULL, NULL, &err);
-    checkCLError(err);
+  // get devices
+  vp[1].getDevices(CL_DEVICE_TYPE_ALL, &vd);
+  printf("device number = %lld\n", vd.size());
+  for (int i = 0; i < vd.size(); ++i) {
+    string info;
+    vd[i].getInfo(CL_DEVICE_NAME, &info);
+    printf("%s\n", info.c_str());
+  }
 
-    // make program
-    //string source = clLoadProgramSource("../PI.cl");
-    string source(PI_SourceCode);
-    vector<pair<const char*, ::size_t>> ps(1, 
-        make_pair(source.c_str(), source.size()));
-    Program program(context, ps, &err);
-    checkCLError(err);
+  // get context
+  cl_context_properties properties[] = {CL_CONTEXT_PLATFORM,
+                                        (cl_context_properties)(vp[1])(), 0};
+  Context context(vd, properties, NULL, NULL, &err);
+  checkCLError(err);
 
-    // build program
-    err = program.build(vd);
-    if(err != CL_SUCCESS)
-    {
-        string errString;
-        checkCLError(program.getBuildInfo(vd[0], 
-            CL_PROGRAM_BUILD_LOG, &errString));
-        printf("%s\n", errString.c_str());
-        exit(1);
-    }
+  // make program
+  string cl_path(CL_PATH);
+  cl_path.append("/PI.cl");
+  string source = clLoadProgramSource(cl_path.c_str());
+  // string source(PI_SourceCode);
+  vector<pair<const char*, ::size_t>> ps(
+      1, make_pair(source.c_str(), source.size()));
+  Program program(context, ps, &err);
+  checkCLError(err);
 
-    // get kernel
-    Kernel pi1(program, "PI1", &err);
-    checkCLError(err);
+  // build program
+  err = program.build(vd);
+  if (err != CL_SUCCESS) {
+    string errString;
+    checkCLError(program.getBuildInfo(vd[0], CL_PROGRAM_BUILD_LOG, &errString));
+    printf("%s\n", errString.c_str());
+    exit(1);
+  }
 
-    Kernel pi2(program, "PI2", &err);
-    checkCLError(err);
+  // get kernel
+  Kernel pi1(program, "PI1", &err);
+  checkCLError(err);
 
-    int groupSize = 128;
-    int num = 1000000;
+  Kernel pi2(program, "PI2", &err);
+  checkCLError(err);
 
-    // get buffer
-    Buffer buf(context, CL_MEM_READ_WRITE, groupSize * sizeof(float));
+  int groupSize = 128;
+  int num = 1000000;
 
-    // make command queue
-    CommandQueue cq(context, vd[0], CL_QUEUE_PROFILING_ENABLE, &err);
-    checkCLError(err);
+  // get buffer
+  Buffer buf(context, CL_MEM_READ_WRITE, groupSize * sizeof(float));
 
-    // enqueue kernel
-    NDRange zero(0);
-    NDRange block(groupSize);
-    NDRange grid(groupSize * groupSize);
+  // make command queue
+  CommandQueue cq(context, vd[0], CL_QUEUE_PROFILING_ENABLE, &err);
+  checkCLError(err);
 
-    checkCLError(pi1.setArg(0, num));
-    checkCLError(pi1.setArg(1, __local(groupSize * sizeof(float))));
-    checkCLError(pi1.setArg(2, buf));
-    checkCLError(cq.enqueueNDRangeKernel(pi1, zero, grid, block));
+  // enqueue kernel
+  NDRange zero(0);
+  NDRange block(groupSize);
+  NDRange grid(groupSize * groupSize);
 
-    checkCLError(pi2.setArg(0, groupSize));
-    checkCLError(pi2.setArg(1, __local(groupSize * sizeof(float))));
-    checkCLError(pi2.setArg(2, buf));
-    checkCLError(cq.enqueueNDRangeKernel(pi2, zero, block, block));
+  checkCLError(pi1.setArg(0, num));
+  checkCLError(pi1.setArg(1, Local(groupSize * sizeof(float))));
+  checkCLError(pi1.setArg(2, buf));
+  checkCLError(cq.enqueueNDRangeKernel(pi1, zero, grid, block));
 
-    // finish queue
-    float r;
-    checkCLError(cq.enqueueReadBuffer(buf, true, 0, sizeof(float), &r));
-    checkCLError(cq.finish());
+  checkCLError(pi2.setArg(0, groupSize));
+  checkCLError(pi2.setArg(1, Local(groupSize * sizeof(float))));
+  checkCLError(pi2.setArg(2, buf));
+  checkCLError(cq.enqueueNDRangeKernel(pi2, zero, block, block));
 
-    printf("%f\n", r);
+  // finish queue
+  float r;
+  checkCLError(cq.enqueueReadBuffer(buf, true, 0, sizeof(float), &r));
+  checkCLError(cq.finish());
 
-    return 0;
+  printf("%f\n", r);
+
+  return 0;
 }
